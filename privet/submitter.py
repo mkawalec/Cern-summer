5e851736d3cc108efaf995771f306b8c04fb6a93
Michal Kawalec <mk.kawalec@gmail.com>
2011-07-14 11:47:22 +0100

    Added file uploading and status checking to status.py

--- privet/submitter.py
+++ privet/submitter.py
@@ -15,9 +15,26 @@ def LaunchJobs(hosts):
 
 def LaunchSsh(host, threads):
 	from os import system
-	output = system("ssh " + host + " && mkdir batchJob ; cd batchJob && make.py --threads " + str(threads))
-	print output:
+	output = system("ssh " + host + " \' mkdir batchJob \'")
+	print output
+	
+	output = system("scp make.py " + host + ":~/batchJob")
+	print output
+	
+	output = system("ssh " + host + " \' cd batchJob ; ./make.py --threads " + threads + "\'")
+	print output
 
+def StatusPoll(hosts, statusFile):
+	for host in hosts:
+		from os import system
+		output = system("scp " + host + ":batchJob/" + statusFile + " .status")
+		print output
+
+		status = open(".status", "r")
+		return "Status of " + host + " is " + str(status.readline())
+		
+
+import sys
 if sys.version_info[:3] < (2, 4, 0):
 	print 'At least python 2.4 is required to launch this script.'
 	system.exit(1)
@@ -26,17 +43,21 @@ if sys.version_info[:3] < (2, 4, 0):
 from optparse import OptionParser
 parser = OptionParser(usage=__doc__, version='1')
 
-parser.add_option('-f' '--file' '--hosts', dest='HOSTS', 
-	default=None, help="Specifies a custom hosts file")
+parser.add_option('-f', '--hosts', dest='HOSTS', default=None, help="Specifies a custom hosts file")
 (opts, args) = parser.parse_args()
 
+import time
 # Check if the custom hosts file was set by the user, if not, use autodetect
 hosts = []
 if not opts.HOSTS:
 	from glob import glob
 	hosts = glob("*.hosts")
 	LaunchJobs(hosts)
+	time.sleep(15)
+	print StatusPoll(hosts, ".status")
 else:
 	hosts = [] * 1
 	hosts[0] = opts.HOSTS
 	LauchJobs(hosts)
+	time.sleep(15)
+	print StatusPoll(hosts, ".status")
