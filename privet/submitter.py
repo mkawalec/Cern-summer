906c98491614de9bcd98133582460583cbcc823c
Michal Kawalec <mk.kawalec@gmail.com>
2011-07-14 12:27:10 +0100

    Making the submitter work in multiprocesses mode to connect to different hosts

--- privet/submitter.py
+++ privet/submitter.py
@@ -6,38 +6,53 @@ and launches the job with the specified number of threads.
 Alternatively another input file can be given from command line
 """
 def LaunchJobs(hosts):
-	for host in hosts:
-		with open(host, 'r') as file:
-			for line in file:
-				line = line.strip()
-				index = line.find(" ")
-				LaunchSsh(line[:index], line[index:].strip())
+    from multiprocessing import Process
+
+    subprocess = []
+    for host in hosts:
+        with open(host, 'r') as file:
+            for line in file:
+                line = line.strip()
+                index = line.find(" ")
+                subprocess.append(Process(target=LaunchSsh, args=(line[:index], line[index:].strip(),)))
+    for process in subprocess:
+        process.start()
+        process.join()
 
 def LaunchSsh(host, threads):
-	from os import system
-	output = system("ssh " + host + " \' mkdir batchJob \'")
-	print output
-	
-	output = system("scp make.py " + host + ":~/batchJob")
-	print output
-	
-	output = system("ssh " + host + " \' cd batchJob ; ./make.py --threads " + threads + "\'")
-	print output
+    from os import system
+    output = system("ssh " + host + " \' mkdir batchJob \'")
+    print output
+    
+    output = system("scp make.py " + host + ":~/batchJob")
+    print output
+    
+    output = system("ssh " + host + " \' cd batchJob ; ./make.py --threads " + threads + "\'")
+    print output
 
-def StatusPoll(hosts, statusFile):
-	for host in hosts:
-		from os import system
-		output = system("scp " + host + ":batchJob/" + statusFile + " .status")
-		print output
+    # Checking if the job is finished
+    output = -1
+    import time
+    while output < 0:
+        time.sleep(30)
+        output = StatusPoll(host, ".status")
+        print "Status of host " + host + "is:" + output
 
-		status = open(".status", "r")
-		return "Status of " + host + " is " + str(status.readline())
-		
+
+
+def StatusPoll(host, statusFile):
+    from os import system
+    output = system("scp " + host + ":batchJob/" + statusFile + " .status")
+    print output
+
+    status = open(".status", "r")
+    return str(status.readline())
+        
 
 import sys
 if sys.version_info[:3] < (2, 4, 0):
-	print 'At least python 2.4 is required to launch this script.'
-	system.exit(1)
+    print 'At least python 2.4 is required to launch this script.'
+    system.exit(1)
 
 # A bit of parsing:
 from optparse import OptionParser
@@ -50,14 +65,10 @@ import time
 # Check if the custom hosts file was set by the user, if not, use autodetect
 hosts = []
 if not opts.HOSTS:
-	from glob import glob
-	hosts = glob("*.hosts")
-	LaunchJobs(hosts)
-	time.sleep(15)
-	print StatusPoll(hosts, ".status")
+    from glob import glob
+    hosts = glob("*.hosts")
+    LaunchJobs(hosts)
 else:
-	hosts = [] * 1
-	hosts[0] = opts.HOSTS
-	LauchJobs(hosts)
-	time.sleep(15)
-	print StatusPoll(hosts, ".status")
+    hosts = [] * 1
+    hosts[0] = opts.HOSTS
+    LauchJobs(hosts)
