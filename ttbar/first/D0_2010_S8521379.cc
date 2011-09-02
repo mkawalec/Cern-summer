3dc96de0272042a06e5ddbf03357ea0974e1f0c5
Dave Mallows <dave.mallows@gmail.com>
2011-09-02 14:31:57 +0100

    Errors aplenty...

--- ttbar/first/D0_2010_S8521379.cc
+++ ttbar/first/D0_2010_S8521379.cc
@@ -1,6 +1,8 @@
 #include "Rivet/Analysis.hh"
 #include "Rivet/Projections/FinalState.hh"
 #include "Rivet/Projections/ChargedLeptons.hh"
+#include "Rivet/Projections/MissingMomentum.hh"
+#include "Rivet/Projections/HadronicFinalState.hh"
 #include "Rivet/Projections/FastJets.hh"
 #include "Rivet/AnalysisLoader.hh"
 #include "Rivet/RivetAIDA.hh"
@@ -22,7 +24,11 @@ namespace Rivet {
     //@{
 
     void init() {
+
       addProjection(ChargedLeptons(FinalState(-1.1, 1.1, 30*GeV)), "LFS");
+
+      addProjection(MissingMomentum(FinalState(-1.1, 1.1, 30*GeV)), "MM");
+
       addProjection(FastJets(FinalState(-5, 5, 0*GeV), FastJets::ANTIKT, 0.4), "Jets");
 
       _h_t_pT_W_cut = bookHistogram1D(2,1,1);
@@ -35,6 +41,8 @@ namespace Rivet {
 
       const ChargedLeptons& lfs = applyProjection<ChargedLeptons>(event, "LFS");
 
+      const MissingMomentum& missmom = applyProjection<MissingMomentum>(event, "MM");
+
       const FastJets& jetpro = applyProjection<FastJets>(event, "Jets");
 
       MSG_DEBUG("Charged lepton multiplicity = " << lfs.chargedLeptons().size());
@@ -44,8 +52,7 @@ namespace Rivet {
         MSG_DEBUG("Lepton pT = " << lepton.momentum().pT());
       }*/
 
-      /// This works with no problems
-      if (lfs.chargedLeptons().empty()) {
+      if (lfs.chargedLeptons().size() != 1) {
         MSG_DEBUG("Event failed lepton multiplicity cut");
         vetoEvent;
       }
@@ -76,36 +83,23 @@ namespace Rivet {
       _multiplicity->fill(ljets.size(), 1);
 
       MSG_DEBUG("Number of b-jets = " << bjets.size());
-      if (bjets.size() < 1) {
+      if (bjets.size() != 2) {
         MSG_DEBUG("Event failed b-tagging cut");
         vetoEvent;
       }
 
-      MSG_DEBUG("Number of l-jets = " << ljets.size());
       if (ljets.size() != 2) {
         MSG_DEBUG("Event failed l-tagging cut");
         vetoEvent;
       }
 
-      size_t blah1 = 0; size_t blah2 = 0; int Wmass = -1000000;
-      for(size_t i = 0; i < ljets.size(); ++i){
-        for(size_t j = 0; j < i; ++j) {
-          if(i != j) {
-            if(abs((ljets[i].momentum().mass() + ljets[i].momentum().mass())/GeV - 80.4) < 
-               abs(Wmass - 80.4)){
-              Wmass = (ljets[i].momentum().mass() + ljets[i].momentum().mass())/GeV;
-              blah1 = i;
-              blah2 = j;
-            }
-          }
-        }
-      }
-      const FourMomentum W  = ljets[blah1].momentum() + ljets[blah2].momentum();
+      const FourMomentum W  = ljets[0].momentum() + ljets[1].momentum();
+      // Or we could veto if ljet multiplicity != 2 ...
 
       if (W.mass()/GeV < 90 && W.mass()/GeV > 70) {
         MSG_DEBUG("W found with mass " << W.mass()/GeV << " GeV");
         const FourMomentum t = W + bjets[0].momentum();
-        std::cout << "Found W! t_pT = "<< t.pT() << ", weight= "<< weight;
+        // std::cout << "Found W! t_pT = "<< t.pT() << ", weight= "<< weight;
         cout << "Ljets number = " << ljets.size() << endl;
 	      _h_t_pT_W_cut->fill(t.pT(), weight);
       }
@@ -113,7 +107,8 @@ namespace Rivet {
         vetoEvent;
       }
 
-      /// @todo Add reconstruction of the other top from the leptonically decaying W, using WFinder
+      double lWmass = mass(missmom.visibleMomentum() + lfs.chargedLeptons().at(0).momentum());
+      MSG_INFO("leptonic W mass: " << lWmass);
     }
 
     void finalize() {
--- ttbar/first/make.sh
+++ ttbar/first/make.sh
@@ -1,9 +1,5 @@
-rivet-buildplugin RivetPlugin.so *.cc
 ANA=D0_2010_S8521379
-
-mkfifo hepmc.fifo
-
-agile-runmc Pythia6:425 --beams=ppbar:1.96T -P params.params -n 100k -o- | RIVET_ANALYSIS_PATH=$PWD rivet -a D0_2010_S8521379
-
-compare-histos Rivet.aida
+rivet-buildplugin RivetPlugin.so *.cc &&\
+agile-runmc Pythia6:425 --randomize-seed --beams=ppbar:1.96T -P params.params -n 100k -o- | RIVET_ANALYSIS_PATH=$PWD rivet -a D0_2010_S8521379 &&\
+compare-histos Rivet.aida &&\
 make-plots *.dat --png
