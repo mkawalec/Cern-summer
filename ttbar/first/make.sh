2baa5cde9f2a5980e38e080a4acd6816c612064a
Dave Mallows <dave.mallows@gmail.com>
2011-09-02 18:25:06 +0200

    Fixed make.sh

--- ttbar/first/make.sh
+++ ttbar/first/make.sh
@@ -1,7 +1,3 @@
-
-mkfifo hepmc.fifo
-
-agile-runmc Pythia6:425 --beams=ppbar:1.96T -P params.params -n 10k -o- | RIVET_ANALYSIS_PATH=$PWD rivet -a D0_2010_S8521379
-
-compare-histos Rivet.aida
+agile-runmc Pythia6:425 --beams=ppbar:1.96T -P params.params -n 10k -o- | RIVET_ANALYSIS_PATH=$PWD rivet -a D0_2010_S8521379 &&\
+compare-histos Rivet.aida &&\
 make-plots --png *.dat
