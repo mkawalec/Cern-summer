b02f91f94103aaecb4ba983055b3b8b434969165
Dave Mallows <dave.mallows@gmail.com>
2011-10-23 20:18:28 +0100

    Added professor run :)

---
+++ ttbar/profrun/017/batch.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+fifo=`mktemp`
+mkfifo ${fifo}.tmp
+mv ${fifo}.tmp ${fifo}
+source /afs/cern.ch/user/d/dmallows/batchenv.sh
+cd /afs/cern.ch/user/d/dmallows/mc/017
+agile-runmc Pythia6:425 --filter=1 --randomize-seed --beams=LHC:7T -P agileparams.params -n 1M -o $fifo &
+rivet -a MC_TTBAR_SEMILEPTONIC -H out.aida $fifo
+rm ${fifo}
